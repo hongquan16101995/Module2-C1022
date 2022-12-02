d9e1fbf487bdab1c10ab4ac8bbc7f3e8d45e7739
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2022-12-02 15:06:10 +0700

    bai 1

---
+++ Demo.java
@@ -0,0 +1,23 @@
+//class: quy định đây là 1 lớp trong chương trình Java đang viết
+//Demo: tên lớp đang viết
+public class Demo {
+    //public: quy định về phạm vi sử dụng của thành phần được đánh dấu
+    //static: từ khóa quy định thành phần static, là thành phần thuộc về lớp
+    //void: kiểu dữ liệu trả về của phương thức
+    //main: tên của phương thức
+    //String[] args: tham số của phương thức;
+    //args = arguments; String[]: kiểu dữ liệu mảng String
+
+    //javac Demo: chuyển từ mã .java => .class
+    //java Demo.class: chạy file bytecode của Java trong cmd
+    //psvm / main / public static void main(String[] args): được gọi là siêu main của 1 chương trình Java
+    //là thành phần được JVM tìm đến đầu tiên khi khởi chạy chương trình Java
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        Test.main(new String[]{"a"});
+    }
+
+    public int demo() {
+        return 1;
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.lang.String;
+import java.lang.System;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = new String("a");
+        System.out.println("HelloWorld");
+
+        String a = "a";
+        char b = 'a';
+        String c = "87";
+        System.out.println(c);
+    }
+}
